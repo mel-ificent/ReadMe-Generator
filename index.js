f7d77e72c1f420b33d38161b35bb8da54e239849
Narcissa <91091964+mel-ificent@users.noreply.github.com>
2022-01-07 17:54:01 -0500

    updated helper functions

--- index.js
+++ index.js
@@ -1,14 +1,72 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-//const questions = [];
+// Creates questions for user input to generate README
+const questions = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a brief description of your project.',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide any installation instructions.',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide any usage instructions.',
+      },
+      {
+        type: 'input',
+        name: 'testing',
+        message: 'Please provide any testing instructions.',
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please provide any contribution guidelines.',
+      },
 
-// TODO: Create a function to write README file
-//function writeToFile(fileName, data) {}
+      {
+        type: 'list',
+        message: 'What license would you like to apply to your project?',
+        name: 'license',
+        choices: ['', 'MIT License', 'GNU GPLv3'],
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+      }
+    ]);
+  };
 
+// function to write README file
+function writeToFile(fileName, data) {
+const readme = generateMarkdown(data);
+}
 // TODO: Create a function to initialize app
-//function init() {}
+function init(){
+
+    questions()
+    .then((answers) => fs.writeToFile('README.md', generateMarkdown.generateMarkdown(answers)))
+    .then(() => console.log('Successfully created README.md'))
+    .catch((err) => console.error(err));
+};
 
 // Function call to initialize app
-//init();
+init();
